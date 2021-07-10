4e954e7b94752e394bf813115beb1cad8934e9f1
powerar <arpower@bell.net>
2021-07-10 14:41:34 -0400

    added contact form component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Work from './components/Work';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Work></Work>
         <About></About>
+        <ContactForm></ContactForm>
       </main>
     </div>
   );
