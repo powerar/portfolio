9c2cb823863c2a6fe8e0847b52075df82dfea6fe
powerar <arpower@bell.net>
2021-07-10 14:37:51 -0400

    added work component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Work from './components/Work';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Work></Work>
         <About></About>
       </main>
     </div>
---
+++ src/components/Work/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function Work () {
+    return (
+        <div>
+            <div>
+                <div>
+                    <link href="https://damp-tor-80298.herokuapp.com/" />
+                    <h3>Gamer Match Maker</h3>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <link href="https://powerar.github.io/horiseon/" />
+                        
+            
+                    <h3>Horiseon</h3>
+                </div>
+                <div>
+                    <link href="https://powerar.github.io/robot-gladiators/" />
+                        
+
+                    <h3>Robot-Gladiators</h3>
+                </div>
+                <div>
+                    <a href="https://powerar.github.io/workday-scheduler/">
+                        
+                    </a>
+                    <h3>Work Day Scheduler</h3>
+                </div>
+                <div>
+                    <link href="https://powerar.github.io/taskmaster-pro/" />
+                    
+                    <h3>Taskmaster Pro</h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Work;
