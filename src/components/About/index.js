43d45b587af41e11319be1a845706dc0354c3bf6
powerar <arpower@bell.net>
2021-07-11 14:35:19 -0400

    added styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
 function About() {
-    return (
-        <section>
-            <div>
-        <section id="about-me" className="section-title col-3">
-            <h3>About Me</h3>
-        </section>
-        <p className="section-text col-9">
-            My name is Andrew Power and I am eager to begin a career in the exciting and ever-changing field of web
-            development. My years of experience in the application support sector gives me a unique perspective on the
-            importance of the user experience. Throughout my career I have developed exceptional client-handling skills
-            as well as the ability to meet tight deadlines and engage in creative problem-solving. I love to learn
-            and I bring this enthusiasm to every project.
-        </p>
-    </div>
-        </section>
-    )
-};
+  return (
+    <section>
+      <div class="card text-center bg-dark text-light">
+        <h3 className="card-header bg-primary">About Me</h3>
+        <body className="bg-dark text-light">
+          <p className="card-text">
+            My name is Andrew Power and I am eager to begin a career in the
+            exciting and ever-changing field of web development. My years of
+            experience in the application support sector gives me a unique
+            perspective on the importance of the user experience. Throughout my
+            career I have developed exceptional client-handling skills as well
+            as the ability to meet tight deadlines and engage in creative
+            problem-solving. I love to learn and I bring this enthusiasm to
+            every project.
+          </p>
+        </body>
+      </div>
+    </section>
+  );
+}
 
 export default About;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,61 @@
 import React, { useEffect } from 'react';
 
-function Nav (props) {
+function Nav(props) {
   const { setCurrentCategory, currentCategory } = props;
 
   useEffect(() => {
-      document.title = currentCategory;
+    document.title = currentCategory;
   }, [currentCategory]);
 
   return (
-    <header className="d-flex flex-wrap bg-dark text-light">
+    <header className="d-flex flex-wrap justify-content-center bg-dark text-light">
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+        integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+        crossorigin="anonymous"
+      ></link>
+      <link rel="stylesheet" href="../../index.css"></link>
       <h1>ANDREW POWER</h1>
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container-fluid">
-          <ul>
+      <nav className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <div className="container">
+          <ul className="nav nav-pills">
             <li>
-              <a href="#about" onClick={() => setCurrentCategory("About")}>About Me</a>
+              <a
+                href="#about"
+                className="btn btn-primary"
+                onClick={() => setCurrentCategory('About')}
+              >
+                About Me
+              </a>
             </li>
             <li>
-              <a href="#work" onClick={() => setCurrentCategory("Work")}>Work</a>
+              <a
+                href="#work"
+                className="btn btn-primary"
+                onClick={() => setCurrentCategory('Work')}
+              >
+                Work
+              </a>
             </li>
             <li>
-              <a href="#contact" onClick={() => setCurrentCategory("Contact")}>Contact Me</a>
+              <a
+                href="#contact"
+                className="btn btn-primary"
+                onClick={() => setCurrentCategory('Contact')}
+              >
+                Contact Me
+              </a>
             </li>
             <li>
-              <a href="#resume" onClick={() => setCurrentCategory("Resume")}>Resume</a>
+              <a
+                href="#resume"
+                className="btn btn-primary"
+                onClick={() => setCurrentCategory('Resume')}
+              >
+                Resume
+              </a>
             </li>
-            
           </ul>
         </div>
       </nav>
