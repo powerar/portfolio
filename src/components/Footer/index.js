52c6786f0abfd39ed9a5183a52af12a976da51be
powerar <arpower@bell.net>
2021-08-10 14:43:05 -0400

    cleaned up card formatting and fixed issue with card reveals

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -31,10 +31,7 @@ function Footer() {
       </div>
       <div class="footer-copyright">
         <div class="container">
-          © 2014 Copyright Text
-          <a class="grey-text text-lighten-4 right" href="#!">
-            More Links
-          </a>
+          © 2021
         </div>
       </div>
     </footer>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -26,10 +26,10 @@ const Project = () => {
   ];
 
   return (
-    <div className="container" id="card-container">
+    <div className="row" id="card-container">
       {projects.map((project, i) => (
-        <div class="card">
-          <div class="card-image waves-effect waves-block waves-light">
+        <div className="card">
+          <div className="card-image waves-effect waves-block waves-light">
             <img
               src={require(`../../images/${projects[i].title}.png`).default}
               className="activator"
@@ -38,10 +38,9 @@ const Project = () => {
             />
           </div>
           <div className="card-content">
-            
-              <span class="card-title activator grey-text text-darken-4">
+              <span className="card-title activator">
                 {projects[i].title}
-                <i class="material-icons right">more_vert</i>
+                <i className="material-icons right">more_vert</i>
               </span>
               <p>
                 <a href={projects[i].appLink}>App</a>
@@ -50,15 +49,14 @@ const Project = () => {
                 <a href={projects[i].github}>Github</a>
               </p>
             </div>
-            <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">
+            <div className="card-reveal">
+              <span className="card-title">
                 {projects[i].title}
-                <i class="material-icons right">close</i>
+                <i className="material-icons">close</i>
               </span>
               <p>{projects[i].description}</p>
             </div>
           </div>
-        
       ))}
     </div>
   );
