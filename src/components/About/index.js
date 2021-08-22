8e26b784655b32e5db36ae905aee12d5bfa95c3b
powerar <arpower@bell.net>
2021-08-22 10:32:02 -0400

    changed about text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
       <div className="center" id="about-text">
         <h3 className="white-text">Hello! My name is Andrew Power</h3>
         <h4 className="center-align">
-         I am a full-stack developer with a lot of enterprise support experience. I'm a developer who understands the importance of the client experience.
+         I am a full-stack developer with extensive enterprise support experience. A developer who understands the client mindset - fast, simple, and effective.
         </h4>
       </div>
     </div>
