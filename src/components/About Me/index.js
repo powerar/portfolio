5e98345603aebbbee7dd5ae29cb3f779057fefe8
powerar <arpower@bell.net>
2021-08-26 18:27:20 -0400

    fixed background gradient and updated about me

--- src/components/About Me/index.js
+++ src/components/About Me/index.js
@@ -5,14 +5,14 @@ function About() {
         <div className="col s4" id="about-text">
         <strong><h3 className="white-text">About Me</h3></strong>
         <h5 className="center-align">
-         I have a combined experience of 8 years as an enterprise application specialist. Through this work I have been able to understand a lot about the client experience.
-         In my work I have developed exceptional client-handling, problem-solving,  and the ability to keep cool under pressure. For this work I have employee awards
-         and delivered best-in-class customer service and NPS scores.
+         I have of 8 years experience as an enterprise application specialist. In my role I have acquired a wealth of knowledge about the client experience.
+         As a result, I have developed exceptional client-handling skills, creative problem-solving, and the ability to deal with tight deadlines. For this work I 
+         have won employee awards and delivered best-in-class customer service and NPS scores.
         <br></br>
         <br></br>
-         Enterprise support and my natural inclination to want to know why and how things work has pushed me into web development. My experience in support and coding bootcamps
-         have given me skills in the AGILE development cycle, troubleshooting and logging bugs, entering enhancement requests, and debugging code. As my experience outlines, I have
-         excellent knowledge in the MERN-stack, and especially in back-end systems and database development.
+         Enterprise support and my desire to want to know why and how things work has pushed me into web development. This experience combined with coding bootcamps
+         has given me proficiencies in the AGILE development cycle, troubleshooting and debugging, and logging enhancement requests. I have
+         excellent knowledge in the MERN-stack, and especially in server-side configuration and database development.
         </h5>
         <br></br>
       </div>
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Hero() {
   return (
       <div className="col s4" id="hero-text">
-        <strong><h3 className="white-text">Hello! My name is Andrew Power</h3></strong>
+        <strong><h3 className="white-text">Hello! My name is Andrew Power.</h3></strong>
         <h5 className="center-align">
-         I am a full-stack developer with extensive enterprise support experience. A developer who understands the client mindset - fast, simple, and effective.
+         I am a full-stack developer with extensive enterprise support experience - a developer who understands the client mindset.
         </h5>
       </div>
   
